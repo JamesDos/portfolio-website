c12a8c1d434d2aac404dde6bddd8f048b5808b23
James Tu <jamestvvo@gmail.com>
2024-09-23 10:41:52 -0400

    add rubiks cube animation

---
+++ src/components/Rubiks/rubiks.tsx
@@ -0,0 +1,42 @@
+import { useRef } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { Mesh } from "three";
+import * as THREE from 'three';
+
+
+const MeshComponent = () => {
+  const cubeMesh = useRef<THREE.Group>(null)
+  const gltf = useLoader(GLTFLoader, "/models/rubiks_cube/scene.gltf")
+
+  useFrame(() => {
+    if (cubeMesh.current) {
+      cubeMesh.current.rotation.y += 0.01
+    }
+  })
+
+  return (
+    <group ref={cubeMesh}>
+      <primitive 
+        object={gltf.scene} 
+        scale={0.3} 
+        rotation={[Math.PI / 4, 0, Math.PI / 4]}
+        position={[0.35, 0, -0.4]}
+        />  
+    </group>
+
+  )
+}
+
+export const RubiksCube = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+    <Canvas className='h-2xl w-2xl'>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <MeshComponent />
+    </Canvas>
+  </div>
+  )
+}
