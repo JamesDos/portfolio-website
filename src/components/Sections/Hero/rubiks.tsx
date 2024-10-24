db6bf1577b6ecf521094a0c4a484ab78c52845bd
James Tu <jamestvvo@gmail.com>
2024-10-23 20:09:52 -0400

    get rid of unused imports

--- src/components/Sections/Hero/rubiks.tsx
+++ src/components/Sections/Hero/rubiks.tsx
@@ -1,8 +1,8 @@
 import { useRef, useEffect } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+// import { OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { Mesh } from "three";
+// import { Mesh } from "three";
 import * as THREE from 'three';
 
 
